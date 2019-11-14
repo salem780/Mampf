434a612f32bde34729031c5c8d998f430f052418
salina <salina@DESKTOP-67881O4>
2019-11-14 11:35:44 +0100

    salina

--- javaskript/jssalina.js
+++ javaskript/jssalina.js
@@ -1,1 +1,10 @@
-gfygfdygdsg<dgdsf
+function Login() {
+
+
+    var account = new Object();
+    account.email = document.getElementById('email').value;
+    account.password = document.getElementById('password').value;
+}
+
+var presstologin = Login.getElementByID('buttongrey');
+
