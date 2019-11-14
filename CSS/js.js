73a9e3d4131bdf4e08105f492436f9883be9a4d2
salina <salina@DESKTOP-67881O4>
2019-11-14 10:49:27 +0100

    taestdsfaf

---
+++ CSS/js.js
@@ -0,0 +1,14 @@
+/*This is a comment */ 
+console.log("Hello again");
+
+let x = 1; 
+x = 'Katze' ;
+//x = true;
+x = 1.1
+
+
+console.log ("datatype:" , typeof x); 
+console.log ("datatype:" , typeof x); 
+
+x= new Date ();
+x
